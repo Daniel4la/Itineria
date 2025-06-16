89110b27a854fdad29f6c48b46b7a8318e20a71a
Daniel4la <dla66772@gmail.com>
2025-06-16 21:42:21 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Itineria
+  Itineria, a travel app designed to enhance and simplify the travel planning experience
---
+++ a2-S3909873-S3915181/Model/Contants.swift
@@ -0,0 +1,13 @@
+//
+//  Contants.swift
+//  ChatGPTChat
+//
+//  Created by LiamRMIT on 26/9/2023.
+//
+
+import Foundation
+
+enum Constants {
+    static let OPEN_API_KEY = "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"
+
+}
